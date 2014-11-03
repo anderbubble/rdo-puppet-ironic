5336565630d0dfc77a0c56bd6eeefd5f109d902c
Jonathon Anderson <jonathon.anderson@colorado.edu>
2014-11-03 17:36:11 -0500

    ironic::keystone::auth

---
+++ manifests/ironic.pp
@@ -0,0 +1,7 @@
+class { 'ironic::keystone::auth':
+  region           => 'RegionOne',
+  password         => 'd78fb6dbd2194a9b',
+  public_address   => '128.138.138.152',
+  admin_address    => '128.138.138.152',
+  internal_address => '128.138.138.152',
+}
